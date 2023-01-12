4f116f59c42ea37f7232eacf836a3c02c25ca1d5
struggyyy <115942178+struggyyy@users.noreply.github.com>
2023-01-12 17:53:24 +0100

    Dodaj pliki projektów.

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Threading; // for Thread.Sleep
+namespace Ex_07_P4
+{
+    interface IMoving
+    {
+        bool MakeMove(char[,] startBoard, char[,] gameBoard);
+    }
+    /**********************************************************************/
+    abstract class Player
+    {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+        public bool CheckIfPlayerWon(char[,] gameBoard)
+        {
+            int height = gameBoard.GetLength(0);
+            int width = gameBoard.GetLength(1);
+            
+            if (height != width)
+                throw new Exception("The board is not a square!");
+            
+            // Check rows
+            
+            for (int i = 0; i < height; i++)
+            {
+                int rowSum = 0;
+                for (int j = 0; j < width; j++)
+                {
+                    if (gameBoard[i, j] == Symbol)
+                        rowSum++;
+                }
+                if (rowSum == width)
+                    return true;
+            }
+            // Check columns
+            
+            for (int j = 0; j < width; j++)
+            {
+                int colSum = 0;
+                for (int i = 0; i < height; i++)
+                {
+                    if (gameBoard[i, j] == Symbol)
+                        colSum++;
+                }
+                if (colSum == height)
+                    return true;
+            }
+            // Check diagonals
+            
+            int diagSumA = 0;
+            int diagSumB = 0;
+            
+            for (int k = 0; k < width; k++)
+            {
+                if (gameBoard[k, k] == Symbol)
+                    diagSumA++;
+                if (gameBoard[k, width - 1 - k] == Symbol)
+                    diagSumB++;
+            }
+            if (diagSumA == width || diagSumB == width)
+                return true;
+            
+            // Otherwise, no win yet
+            
+            return false;
+        }
+        public bool PlaceSymbol(char c, char[,] startBoard, char[,] gameBoard)
+        {
+            int height = gameBoard.GetLength(0);
+            int width = gameBoard.GetLength(1);
+            
+            if (height != startBoard.GetLength(0) || width != startBoard.GetLength(1))
+                throw new Exception("The boards have different sizes!");
+            
+            // Try to put player's symbol at a given place, if the place is available
+            
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    if (gameBoard[i, j] == c && gameBoard[i, j] == startBoard[i, j])
+                    {
+                        gameBoard[i, j] = Symbol;
+                        return true;
+                    }
+           
+           // Otherwise, return without success
+           
+           return false;
+        }
+    }
+    /**********************************************************************/
+    class HumanPlayer : Player, IMoving
+    {
+        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
+        {
+            
+           // Ask human player to enter a place until (s)he picks an available one
+           
+            char chosenPlace;
+            do
+            {
+                Console.Write("Choose an empty place: ");
+                chosenPlace = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+            while (!PlaceSymbol(chosenPlace, startBoard, gameBoard));
+
+            return CheckIfPlayerWon(gameBoard);
+        }
+    }
+    /**********************************************************************/
+    class ComputerPlayer : Player, IMoving
+    {
+        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
+        {
+            
+           // Draw random numbers until AI player picks an available one
+           
+            Random rnd = new Random();
+            char chosenPlace;
+            
+            do
+            {
+                int p = rnd.Next(1, gameBoard.Length + 1); // random 1-9
+                chosenPlace = p.ToString()[0]; // convert digit to char
+            }
+            while (!PlaceSymbol(chosenPlace, startBoard, gameBoard));
+            Thread.Sleep(2000); // wait 2 seconds
+            return CheckIfPlayerWon(gameBoard);
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace Ex_07_P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two players
+
+            HumanPlayer p1 = new HumanPlayer();
+            ComputerPlayer p2 = new ComputerPlayer();
+
+            p1.Name = "struggyyy";
+            p2.Name = "AI";
+            p1.Symbol = 'x';
+            p2.Symbol = 'o';
+            
+            // Create two boards - with starting and with current fields
+            
+            char[,] startBoard = {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }};
+
+            char[,] gameBoard = startBoard.Clone() as char[,];
+
+            // Flags
+
+            bool player1Won = false;
+            bool player2Won = false;
+            bool nextIsPlayer1 = true; 
+            
+            // true - player 1 move, false - player 2 move
+                                       
+            /////////////////////////////////////////////////////////
+                                       
+            // Loop over rounds
+
+            for (int round = 0; round < gameBoard.Length; round++)
+            {
+                Console.Clear();
+                Draw(gameBoard);
+                if (nextIsPlayer1)
+                {
+                    Console.WriteLine(p1.Name + " move");
+                    player1Won = p1.MakeMove(startBoard, gameBoard);
+                    nextIsPlayer1 = false;
+                }
+                else
+                {
+                    Console.WriteLine(p2.Name + " move");
+                    player2Won = p2.MakeMove(startBoard, gameBoard);
+                    nextIsPlayer1 = true;
+                }
+                if (player1Won || player2Won)
+                    break;
+            }
+            //////////////////////////////////////////////////////////
+            ///
+            // End the game
+
+            Console.Clear();
+            Draw(gameBoard);
+
+            Console.Write("Game ended! ");
+
+            if (player1Won)
+                Console.WriteLine("Winner: " + p1.Name);
+
+            else if (player2Won)
+                Console.WriteLine("Winner: " + p2.Name);
+
+            else
+                Console.WriteLine("A tie!");
+        }
+        /**********************************************************/
+        static void Draw(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                    Console.Write(board[i, j]);
+                
+             Console.WriteLine();
+            }
+        }
+    }
+}
